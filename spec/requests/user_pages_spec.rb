57090129747b9c6afce6aef8cc842dfa8a5028bc
Kelvin Tang <yihuitang@gmail.com>
2012-10-19 10:49:45 +0800

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,8 +8,8 @@ describe "User pages" do
     let(:user) {FactoryGirl.create(:user)}
     before { visit user_path(user) }
 
-    it { should have_selector('title', text: user.name)  }
-    it { should have_selector('h1', text: user.email) }
+    it { should have_selector('h1', text: user.name)  }
+    it { should have_selector('title', text: user.name) }
   end
   
   describe "signup page" do
@@ -39,6 +39,20 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+
+      describe "followed by signout"
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
       end
     end  
   end
