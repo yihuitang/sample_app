1baba7af65634cecbf917453ccb0285f75287b39
Kelvin Tang <yihuitang@gmail.com>
2012-10-26 19:22:02 +0800

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,9 +25,20 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attibute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)   
+    end
+
+      it { should be_admin }
+  end
 
   describe "when name is not present" do
     before { @user.name = "" }
@@ -115,3 +126,4 @@ describe User do
     its(:remember_token) { should_not be_blank }
   end
 end
+
